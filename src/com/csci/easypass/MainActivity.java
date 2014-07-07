18e67757112008351aa2f25a8c41cc2e8b29288b
Yuqing Jiang <yqjiang0830@gmail.com>
2014-07-07 15:18:42 -0300

    rename_resources

--- src/com/csci/easypass/MainActivity.java
+++ src/com/csci/easypass/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnNextScreen = (Button) findViewById(R.id.button1);
-        Button btnLogin = (Button)findViewById(R.id.button2);
+        Button btnNextScreen = (Button) findViewById(R.id.create_account_btn);
+        Button btnLogin = (Button)findViewById(R.id.login_btn);
         
         btnNextScreen.setOnClickListener(new View.OnClickListener() {
         	 
