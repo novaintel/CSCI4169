8b5d8cf2a7ad91d1d4cc31c16849d70ac97acc6c
James Hill <jaed.llih@gmail.com>
2014-07-21 22:23:27 -0300

    Started to work on the db which will be needed for the both the log in screen and the logged in screen.

---
+++ src/com/csci/easypass/Logedin.java
@@ -0,0 +1,16 @@
+package com.csci.easypass;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by James on 2014-07-14.
+ */
+public class Logedin extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+
+    }
+
+}
