670a58834def2f47d1cd9358de89b043a602ac78
James Hill <jaed.llih@gmail.com>
2014-07-28 23:19:45 -0300

    Referenced the wrong UI button, which would cause the Signup page to crash on, init

--- EasyPass/app/src/main/java/com/csci/easypass/Create_account.java
+++ EasyPass/app/src/main/java/com/csci/easypass/Create_account.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Create_account extends Activity {
@@ -17,35 +18,48 @@ public class Create_account extends Activity {
     TextView passwordTextBox;
     MySQLiteHelper db;
 
-	 protected void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.create_account);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
 
-         usernameTextBox=new TextView(this);
-         usernameTextBox=(TextView)findViewById(R.id.create_username);
+        usernameTextBox = new EditText(this);
+        usernameTextBox = (EditText) findViewById(R.id.create_username);
 
-         passwordTextBox=new TextView(this);
-         passwordTextBox=(TextView)findViewById(R.id.create_password);
+        passwordTextBox = new EditText(this);
+        passwordTextBox = (EditText) findViewById(R.id.create_password);
 
-         passwordAgainTextBox=new TextView(this);
-         passwordAgainTextBox=(TextView)findViewById(R.id.create_reenter_password);
+        passwordAgainTextBox = new EditText(this);
+        passwordAgainTextBox = (EditText) findViewById(R.id.create_reenter_password);
 
-         db = new MySQLiteHelper(this);
+        db = new MySQLiteHelper(this);
 
-         final Button createAccButton = (Button) findViewById(R.id.create_account_btn);
+        Button createAccButton = (Button) findViewById(R.id.sign_up);
 
-         createAccButton.setOnClickListener(new View.OnClickListener() {
-             public void onClick(View v) {
+        createAccButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
                 String username = (String)usernameTextBox.getText();
                 String password = (String)passwordTextBox.getText();
                 String passwordAgain = (String)passwordAgainTextBox.getText();
+                if(username.equals("") || username.equals(" ")){
+                    return;
+                }
+                if(password.equals("") || password.equals(" ")){
+                    return;
+                }
+                if(passwordAgain.equals("") || passwordAgain.equals(" ")){
+                    return;
+                }
                 if(password.equals(passwordAgain)){
                     db.addUser(new User(username, password));
                 }
-             }
-         });
-	        
-	    }
 
+            }
+
+        });
+
+    }
 
 }
